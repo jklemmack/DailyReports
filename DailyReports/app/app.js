ef08920a5609e44b441a9f0d750176e343f50512
Johann Klemmack <johann.klemmack@gmail.com>
2013-09-10 16:26:37 -0700

    Initial copy from recipe book

---
+++ DailyReports/app/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('myApp', ['ui.compat', 'myApp.Recipe'])
+	.config(['$stateProvider', function($stateProvider) {
+				
+	}]);
+/*.run(['$rootScope', '$state', '$stateParams',
+function ($rootScope, $state, $stateParams) {
+$rootScope.$state = $state;
+$rootScope.$stateParams = $stateParams;
+}])*/
---
+++ DailyReports/js/recipes.js
@@ -0,0 +1,30 @@
+ [
+    {
+				"id" : "1",
+        "title": "Honey Oatmeal Bread",
+        "prepTime": "10 minutes",
+        "cookTime": "50 minutes",
+        "flavorText": "An old classic",
+        "ingredients": [
+            {"name": "Yeast", "amount": "1 packet"},
+            {"name": "Flour", "amount": "3 1/2 cups"}
+        ],
+        "instructions": "Mix, kneed, bake",
+        "pictures": ["image001.jpg"],
+        "categories": ["Appetizers", "All"]        
+    },{
+				"id" : "2",
+        "title": "Tomato Soup",
+        "prepTime": "10 minutes",
+        "cookTime": "50 minutes",
+        "flavorText": "An old classic",
+        "ingredients": [
+            {"name": "Yeast", "amount": "1 packet"},
+            {"name": "Flour", "amount": "3 1/2 cups"}
+        ],
+        "instructions": "Mix, kneed, bake",
+        "pictures": ["image001.jpg"],
+        "categories": ["Soups", "All"]        
+        
+    }
+]
